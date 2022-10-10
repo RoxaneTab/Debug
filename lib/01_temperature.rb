7eb8867eeb7b2d4e141c71b40f84a34785d6e65a
RoxaneTab <roxane.t@hotmail.fr>
2022-10-10 22:04:36 +0200

    lib unfinished

---
+++ lib/01_temperature.rb
@@ -0,0 +1,7 @@
+def ftoc(farenheit)
+    (farenheit.to_f - 32) * 5 / 9
+end
+
+def ctof(celsuis)
+    (celsius.to_f * 9 / 5) + 32
+end
---
+++ lib/02_calculator.rb
@@ -0,0 +1,25 @@
+def add(a,b)
+  a + b
+end
+
+def substract(a,b)
+  a - b
+end
+
+def sum[].to_i
+  Array#sum
+end
+
+def multiply(a,b)
+  a * b
+end
+
+def power(a,b)
+  a ** be
+end
+
+
+def factorial(n)
+    n == 0? 1 : (1..n).inject(1) { |total, i| total*= i; total }
+  end
+
